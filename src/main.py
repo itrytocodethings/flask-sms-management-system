dc6aadcd846627f7cccd2472a35ead562b740ce4
itrytocodethings <imwaynebailey@gmail.com>
2022-03-25 23:33:54 +0000

    started exercise

--- src/main.py
+++ src/main.py
@@ -7,18 +7,21 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
-#from models import Person
+# from admin import setup_admin
+# from models import db, User
+from datastructure import Queue
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
-db.init_app(app)
-CORS(app)
-setup_admin(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# MIGRATE = Migrate(app, db)
+# db.init_app(app)
+# CORS(app)
+# setup_admin(app)
+
+db = Queue();
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -30,14 +33,33 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+@app.route('/new', methods=['POST'])
+def new():
+    guest = request.json
+    size = db.size()
+    db.enqueue(guest)
+    responseBody = {
+        "msg": f"Hello, {guest['name']} you've been added, currently there are {size} customers in front of you"
     }
+    return jsonify(responseBody), 200
 
-    return jsonify(response_body), 200
+@app.route('/next', methods=['GET'])
+def next():
+    if db.size() > 0:
+        guest = db.dequeue()
+        responseBody = {
+            "msg": f"Hello, {guest['name']} it is your turn!"
+        }
+    else:
+        responseBody = {
+            "msg": f"The queue is empy."
+        }
+    return jsonify(responseBody), 200
+
+@app.route('/all', methods=['GET'])
+def all():
+    guestQueue = db.get_queue()
+    return jsonify(guestQueue), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+#     def __repr__(self):
+#         return '<User %r>' % self.username
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+
+# class Queue:
+#     def __init__(self):
+#         self._queue = []
+#         # depending on the _mode, the queue has to behave like a FIFO or LIFO
+#         self._mode = 'FIFO'
+#     # def enqueue(self, item):
+#     # def dequeue(self):
+#     # def get_queue(self):
+#     # def size(self):
+#         return len(self._queue) 
