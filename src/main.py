c471a2572825ababd52e7ccdc3bc09b3f5cbc429
itrytocodethings <imwaynebailey@gmail.com>
2022-03-26 00:18:50 +0000

    completed

--- src/main.py
+++ src/main.py
@@ -7,20 +7,20 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
+from sms import sendSMS
 # from admin import setup_admin
 # from models import db, User
-from datastructure import Queue
-# from models import Person
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # MIGRATE = Migrate(app, db)
 # db.init_app(app)
 # CORS(app)
 # setup_admin(app)
-
+from datastructure import Queue
+# from models import Person
+from twilio.rest import Client
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 db = Queue();
 
 # Handle/serialize errors like a JSON object
@@ -41,6 +41,7 @@ def new():
     responseBody = {
         "msg": f"Hello, {guest['name']} you've been added, currently there are {size} customers in front of you"
     }
+    sendSMS(responseBody['msg'], guest['phone'])
     return jsonify(responseBody), 200
 
 @app.route('/next', methods=['GET'])
@@ -50,10 +51,12 @@ def next():
         responseBody = {
             "msg": f"Hello, {guest['name']} it is your turn!"
         }
+        sendSMS(responseBody['msg'], guest['phone'])
     else:
         responseBody = {
             "msg": f"The queue is empy."
         }
+
     return jsonify(responseBody), 200
 
 @app.route('/all', methods=['GET'])
---
+++ src/sms.py
@@ -0,0 +1,18 @@
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+def sendSMS(body='body', to="to"):
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+                                body=body,
+                                from_='+18644005130',
+                                to=to
+                            )
+
+    print(message.sid)
